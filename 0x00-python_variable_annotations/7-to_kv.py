01eb9afe1134ea4b575031f24b6b480817d6c1c9
unkown007 <victormatusse00@gmail.com>
2023-12-07 23:40:19 +0200

    added file

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Define to_kv """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Create a tuple composed of the arguments """
-    return (k, v ** 2)
+    return (k, float(v ** 2))
