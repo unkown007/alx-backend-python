6edc05782e4bff6be57494fbdb23251d6359791a
unkown007 <victormatusse00@gmail.com>
2023-12-07 22:08:10 +0200

    added file

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Define sum_list """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Sum all the elements of the list
+    Args:
+        input_list(list[int]): list of floats
+    Return: sum of all elements
+    """
+    return sum(input_list)
