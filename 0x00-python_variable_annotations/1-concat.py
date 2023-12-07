294d200a2563752676e8318bcc8b8e6774c086b1
unkown007 <victormatusse00@gmail.com>
2023-12-07 21:05:06 +0200

    added file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Define concat method """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate two strings
+    Args:
+        str1(str): first string
+        str2(str): second string
+    Return: Return new concatenated string str1 + str2
+    """
+    return str1 + str2
