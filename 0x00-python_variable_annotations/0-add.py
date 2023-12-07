4fda78b9e526c68af1a97a7e33e5e05cc7f11abb
unkown007 <victormatusse00@gmail.com>
2023-12-07 20:59:48 +0200

    added file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module define add function"""
+
+
+def add(a: float, b: float) -> float:
+    """ Sums two numbers
+    Args:
+        a(float): float number
+        b(float): float number
+    Return: the sum of two float number a and b
+    """
+    return a + b
