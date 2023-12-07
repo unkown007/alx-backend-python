a2d9c4fa002f7505ecdec8a609c29853f377fac1
unkown007 <victormatusse00@gmail.com>
2023-12-07 21:26:08 +0200

    added file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Defines to_str """
+
+
+def to_str(n: float) -> str:
+    """ Convert a float number to a string
+    Args:
+        n(float): float number
+    Returns: string containg a float number
+    """
+    return str(n)
