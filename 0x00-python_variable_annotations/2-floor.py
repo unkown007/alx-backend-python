6b76b08a4982db2320fcd6cf1244105e22143497
unkown007 <victormatusse00@gmail.com>
2023-12-07 21:15:52 +0200

    added file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Define floor method """
+import math
+
+
+def floor(n: float) -> float:
+    """ Computes mathematical floor
+    Args:
+        n(float): float number
+    Return: Floor of a float number
+    """
+    return math.floor(n)
