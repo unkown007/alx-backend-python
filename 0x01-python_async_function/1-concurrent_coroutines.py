2337912ff8229f36937c85a3d4cb240a7bf43f52
unkown007 <victormatusse00@gmail.com>
2023-12-11 22:37:23 +0200

    added file

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Define wait_n """
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Spawn wait_random n times with the specified max_delay
+    Args:
+        n(int): number of time to spawn
+        max_delay(int): maximum delay
+    Return: list of floats of spawn numbers
+    """
+    spawn_number: list = []
+    for _ in range(n):
+        spawn_number.append(await wait_random(max_delay))
+    return spawn_number
