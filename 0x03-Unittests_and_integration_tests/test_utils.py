cc24354a7d6c4be911ee0b1bdd53f1160440f757
unkown007 <victormatusse00@gmail.com>
2024-05-13 21:22:21 +0200

    added file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,11 @@
 """ TestAccessNestedMap module
 """
 from parameterized import parameterized
+from typing import Dict
+from unittest.mock import patch, Mock
 import unittest
 
-
-nested_map = __import__("utils").access_nested_map
+from utils import (nested_map, get_json)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,3 +30,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Raise KeyError when some inputs are expected """
         with self.assertRaises(KeyError, msg=expected):
             nested_map(_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """" Implements the TestGetJson.test_get_json """
+
+    @parametized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict
+            ) -> None:
+        """ Test get_json from utils """
+
+        attrs = {"json.return_value": test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
